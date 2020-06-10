df0b3ca4a7309bc851bf6d9f780e766c22599165
Alex Mabry <alex.mabry@synapse-wireless.com>
2020-06-10 08:26:53 -0500

    Improve UI

--- src/QueryEditor.tsx
+++ src/QueryEditor.tsx
@@ -1,36 +1,56 @@
-import defaults from 'lodash/defaults';
-
-import React, { ChangeEvent, PureComponent } from 'react';
-import { LegacyForms } from '@grafana/ui';
+import React, { ChangeEvent, useEffect } from 'react';
 import { QueryEditorProps } from '@grafana/data';
 import { TimeLionDataSource } from './datasource';
 import { defaultQuery, TimeLionDataSourceOptions, TimeLionQuery } from './types';
 
-const { FormField } = LegacyForms;
-
 type Props = QueryEditorProps<TimeLionDataSource, TimeLionQuery, TimeLionDataSourceOptions>;
 
-export class QueryEditor extends PureComponent<Props> {
-  onTargetChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { onChange, query } = this.props;
+export const QueryEditor = (props: Props) => {
+  const { query, onChange, onRunQuery } = props;
+
+  // Apply some defaults on initial render
+  useEffect(() => {
+    onChange({ ...query, ...defaultQuery });
+  }, []);
+
+  const onTargetChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     onChange({ ...query, target: event.target.value });
   };
 
-  render() {
-    const query = defaults(this.props.query, defaultQuery);
-    const { target } = query;
+  const onIntervalChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onChange({ ...query, interval: event.target.value });
+  };
 
-    return (
-      <div className="gf-form">
-        <FormField
-          labelWidth={8}
-          inputWidth={32}
-          value={target || ''}
-          onChange={this.onTargetChange}
-          label="Expression"
-          tooltip="Timelion expression"
-        />
+  return (
+    <>
+      <div className="gf-form-inline">
+        <div className="gf-form gf-form--grow">
+          <div className="gf-form gf-form--v-stretch">
+            <label className="gf-form-label width-7">Expression</label>
+          </div>
+          <textarea
+            className="gf-form-input gf-form-textarea"
+            placeholder=".es()"
+            value={query.target}
+            onChange={onTargetChange}
+            onBlur={onRunQuery}
+          ></textarea>
+        </div>
       </div>
-    );
-  }
-}
+
+      <div className="gf-form-inline">
+        <div className="gf-form gf-form--grow">
+          <label className="gf-form-label width-7">Interval</label>
+          <input
+            type="text"
+            className="gf-form-input"
+            placeholder="custom interval"
+            value={query.interval}
+            onChange={onIntervalChange}
+            onBlur={onRunQuery}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
